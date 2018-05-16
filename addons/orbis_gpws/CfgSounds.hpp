313545131b44306cb7ca5ad987f9def1ad81f16a
mgkid3310 <mgkid3310@naver.com>
2018-05-17 05:28:57 +0900

    gpws sound length added & volume change

--- addons/orbis_gpws/CfgSounds.hpp
+++ addons/orbis_gpws/CfgSounds.hpp
@@ -1,85 +1,101 @@
-#define F16_VOL 10
-#define B747_VOL 10
+#define F16_VOL 20
+#define B747_VOL 20
 
 class CfgSounds {
 	// F16 GPWS
 	class f16_altitude {
 		name = "F16 Altitude";
+		length = 2.14;
 		sound[] = {"orbis_gpws\sounds\f16\ALTITUDE.ogg", F16_VOL, 1};
 		titles[] = {};
 	};
 	class f16_bingo {
 		name = "F16 Bingo";
+		length = 1.73;
 		sound[] = {"orbis_gpws\sounds\f16\BINGO.ogg", F16_VOL, 1};
 		titles[] = {};
 	};
 	class f16_caution {
 		name = "F16 Caution";
+		length = 1.90;
 		sound[] = {"orbis_gpws\sounds\f16\CAUTION.ogg", F16_VOL, 1};
 		titles[] = {};
 	};
 	class f16_chaffFlare {
 		name = "F16 Chaff Flare";
+		length = 0.86;
 		sound[] = {"orbis_gpws\sounds\f16\CHAFF_FLARE.ogg", F16_VOL, 1};
 		titles[] = {};
 	};
 	class f16_chaffFlareLow {
 		name = "F16 Chaff Flare Low";
+		length = 1.34;
 		sound[] = {"orbis_gpws\sounds\f16\CHAFF_FLARE_LOW.ogg", F16_VOL, 1};
 		titles[] = {};
 	};
 	class f16_chaffFlareOut {
 		name = "F16 Chaff Flare Out";
+		length = 1.26;
 		sound[] = {"orbis_gpws\sounds\f16\CHAFF_FLARE_OUT.ogg", F16_VOL, 1};
 		titles[] = {};
 	};
 	class f16_counter {
 		name = "F16 Counter";
+		length = 0.67;
 		sound[] = {"orbis_gpws\sounds\f16\COUNTER.ogg", F16_VOL, 1};
 		titles[] = {};
 	};
 	class f16_data {
 		name = "F16 Data";
+		length = 0.42;
 		sound[] = {"orbis_gpws\sounds\f16\DATA.ogg", F16_VOL, 1};
 		titles[] = {};
 	};
 	class f16_highAOA {
 		name = "F16 High AOA";
+		length = 0.999;
 		sound[] = {"orbis_gpws\sounds\f16\HIGH_AOA.ogg", F16_VOL, 1};
 		titles[] = {};
 	};
 	class f16_IFF {
 		name = "F16 IFF";
+		length = 0.97;
 		sound[] = {"orbis_gpws\sounds\f16\IFF.ogg", F16_VOL, 1};
 		titles[] = {};
 	};
 	class f16_jammer {
 		name = "F16 Jammer";
+		length = 0.54;
 		sound[] = {"orbis_gpws\sounds\f16\JAMMER.ogg", F16_VOL, 1};
 		titles[] = {};
 	};
 	class f16_lock {
 		name = "F16 Lock";
+		length = 0.61;
 		sound[] = {"orbis_gpws\sounds\f16\LOCK.ogg", F16_VOL, 1};
 		titles[] = {};
 	};
 	class f16_lowSpeed {
 		name = "F16 Low Speed";
+		length = 1.50;
 		sound[] = {"orbis_gpws\sounds\f16\LOW_SPEED.ogg", F16_VOL, 1};
 		titles[] = {};
 	};
 	class f16_pullUp {
 		name = "F16 Pull Up";
+		length = 1.58;
 		sound[] = {"orbis_gpws\sounds\f16\PULL_UP.ogg", F16_VOL, 1};
 		titles[] = {};
 	};
 	class f16_SAM {
 		name = "F16 SAM";
+		length = 0.80;
 		sound[] = {"orbis_gpws\sounds\f16\SAM.ogg", F16_VOL, 1.005};
 		titles[] = {};
 	};
 	class f16_warning {
 		name = "F16 Warning";
+		length = 2.20;
 		sound[] = {"orbis_gpws\sounds\f16\WARNING.ogg", F16_VOL, 1};
 		titles[] = {};
 	};

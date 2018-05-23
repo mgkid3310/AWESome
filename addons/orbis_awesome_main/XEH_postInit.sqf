326c208b84258c04d5100f452f75668ee7948492
Mingi Park <mgkid3310@naver.com>
2018-05-23 21:06:11 +0900

    add main addon (ace compat check)

---
+++ addons/orbis_awesome_main/XEH_postInit.sqf
@@ -0,0 +1,2 @@
+orbis_hasACEInteractMenu = isClass (configFile >> "CfgPatches" >> "ace_interact_menu");
+orbis_hasACEWeather = isClass (configFile >> "CfgPatches" >> "ace_weather");
---
+++ addons/orbis_awesome_main/config.cpp
@@ -0,0 +1,12 @@
+class CfgPatches {
+	class orbis_awesome_main {
+		name = "Orbis AWESome Main";
+		author = "Orbis2358";
+		requiredVersion = 0.1;
+		requiredAddons[] = {};
+		units[] = {};
+		weapons[] = {};
+	};
+};
+
+#include "CfgEventHandlers.hpp"

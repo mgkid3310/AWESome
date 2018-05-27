f2884d2e7a8dc42dae73249f88a060d29ba43bda
mgkid3310 <mgkid3310@naver.com>
2018-05-27 22:04:05 +0900

    add sound lenght data for b747

--- addons/orbis_gpws/CfgSounds.hpp
+++ addons/orbis_gpws/CfgSounds.hpp
@@ -103,151 +103,181 @@ class CfgSounds {
 	// Large planes GPWS (C130 & larger)
 	class b747_10 {
 		name = "Large 10";
+		length = 0.356;
 		sound[] = {"orbis_gpws\sounds\b747\10.ogg", B747_VOL, 1};
 		titles[] = {};
 	};
 	class b747_20 {
 		name = "Large 20";
+		length = 0.413;
 		sound[] = {"orbis_gpws\sounds\b747\20.ogg", B747_VOL, 1};
 		titles[] = {};
 	};
 	class b747_30 {
 		name = "Large 30";
+		length = 0.400;
 		sound[] = {"orbis_gpws\sounds\b747\30.ogg", B747_VOL, 1};
 		titles[] = {};
 	};
 	class b747_40 {
 		name = "Large 40";
+		length = 0.450;
 		sound[] = {"orbis_gpws\sounds\b747\40.ogg", B747_VOL, 1};
 		titles[] = {};
 	};
 	class b747_50 {
 		name = "Large 50";
+		length = 0.472;
 		sound[] = {"orbis_gpws\sounds\b747\50.ogg", B747_VOL, 1};
 		titles[] = {};
 	};
 	class b747_100 {
 		name = "Large 100";
+		length = 0.834;
 		sound[] = {"orbis_gpws\sounds\b747\100.ogg", B747_VOL, 1};
 		titles[] = {};
 	};
 	class b747_200 {
 		name = "Large 200";
+		length = 0.772;
 		sound[] = {"orbis_gpws\sounds\b747\200.ogg", B747_VOL, 1};
 		titles[] = {};
 	};
 	class b747_300 {
 		name = "Large 300";
+		length = 0.960;
 		sound[] = {"orbis_gpws\sounds\b747\300.ogg", B747_VOL, 1};
 		titles[] = {};
 	};
 	class b747_400 {
 		name = "Large 400";
+		length = 0.853;
 		sound[] = {"orbis_gpws\sounds\b747\400.ogg", B747_VOL, 1};
 		titles[] = {};
 	};
 	class b747_500 {
 		name = "Large 500";
+		length = 0.967;
 		sound[] = {"orbis_gpws\sounds\b747\500.ogg", B747_VOL, 1};
 		titles[] = {};
 	};
 	class b747_1000 {
 		name = "Large 1000";
+		length = 0.934;
 		sound[] = {"orbis_gpws\sounds\b747\1000.ogg", B747_VOL, 1};
 		titles[] = {};
 	};
 	class b747_ALTALRT {
 		name = "Large ALTALRT";
+		length = 0.763;
 		sound[] = {"orbis_gpws\sounds\b747\ALTALRT.ogg", B747_VOL, 1};
 		titles[] = {};
 	};
 	class b747_ALTENTR {
 		name = "Large ALTENTR";
+		length = 1.083;
 		sound[] = {"orbis_gpws\sounds\b747\ALTENTR.ogg", B747_VOL, 1};
 		titles[] = {};
 	};
 	class b747_APDISCO {
 		name = "Large APDISCO";
+		length = 1.311;
 		sound[] = {"orbis_gpws\sounds\b747\APDISCO.ogg", B747_VOL, 1};
 		titles[] = {};
 	};
 	class b747_APPRMIN {
 		name = "Large APPRMIN";
+		length = 1.414;
 		sound[] = {"orbis_gpws\sounds\b747\APPRMIN.ogg", B747_VOL, 1};
 		titles[] = {};
 	};
 	class b747_ATTEND {
 		name = "Large ATTEND";
+		length = 1.000;
 		sound[] = {"orbis_gpws\sounds\b747\ATTEND.ogg", B747_VOL, 1};
 		titles[] = {};
 	};
 	class b747_BNKANGL {
 		name = "Large BNKANGL";
+		length = 0.891;
 		sound[] = {"orbis_gpws\sounds\b747\BNKANGL.ogg", B747_VOL, 1};
 		titles[] = {};
 	};
 	class b747_CHIME {
 		name = "Large CHIME";
+		length = 0.612;
 		sound[] = {"orbis_gpws\sounds\b747\CHIME.ogg", B747_VOL, 1};
 		titles[] = {};
 	};
 	class b747_DONTSNK {
 		name = "Large DONTSNK";
+		length = 0.758;
 		sound[] = {"orbis_gpws\sounds\b747\DONTSNK.ogg", B747_VOL, 1};
 		titles[] = {};
 	};
 	class b747_FLAPS {
 		name = "Large FLAPS";
+		length = 1.182;
 		sound[] = {"orbis_gpws\sounds\b747\FLAPS.ogg", B747_VOL, 1};
 		titles[] = {};
 	};
 	class b747_GEAR {
 		name = "Large GEAR";
+		length = 0.941;
 		sound[] = {"orbis_gpws\sounds\b747\GEAR.ogg", B747_VOL, 1};
 		titles[] = {};
 	};
 	class b747_GLIDESLOPE {
 		name = "Large GLIDESLOPE";
+		length = 0.833;
 		sound[] = {"orbis_gpws\sounds\b747\GLIDESLOPE.ogg", B747_VOL, 1};
 		titles[] = {};
 	};
 	class b747_MIN {
 		name = "Large MIN";
+		length = 0.754;
 		sound[] = {"orbis_gpws\sounds\b747\MIN.ogg", B747_VOL, 1};
 		titles[] = {};
 	};
 	class b747_PULLUP {
 		name = "Large PULLUP";
+		length = 1.265;
 		sound[] = {"orbis_gpws\sounds\b747\PULLUP.ogg", B747_VOL, 1};
 		titles[] = {};
 	};
 	class b747_SNKRATE {
 		name = "Large SNKRATE";
+		length = 0.730;
 		sound[] = {"orbis_gpws\sounds\b747\SNKRATE.ogg", B747_VOL, 1};
 		titles[] = {};
 	};
 	class b747_TERRAIN {
 		name = "Large TERRAIN";
+		length = 0.511;
 		sound[] = {"orbis_gpws\sounds\b747\TERRAIN.ogg", B747_VOL, 1};
 		titles[] = {};
 	};
 	class b747_TOOLOWT {
 		name = "Large TOOLOWT";
+		length = 1.007;
 		sound[] = {"orbis_gpws\sounds\b747\TOOLOWT.ogg", B747_VOL, 1};
 		titles[] = {};
 	};
 	class b747_TOWARN {
 		name = "Large TOWARN";
+		length = 1.751;
 		sound[] = {"orbis_gpws\sounds\b747\TOWARN.ogg", B747_VOL, 1};
 		titles[] = {};
 	};
 	class b747_TRIM {
 		name = "Large TRIM";
+		length = 0.686;
 		sound[] = {"orbis_gpws\sounds\b747\TRIM.ogg", B747_VOL, 1};
 		titles[] = {};
 	};
 	class b747_WINDSHR {
 		name = "Large WINDSHR";
+		length = 3.000;
 		sound[] = {"orbis_gpws\sounds\b747\WINDSHR.ogg", B747_VOL, 1};
 		titles[] = {};
 	};

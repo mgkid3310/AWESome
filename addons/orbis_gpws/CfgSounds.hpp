5c4daf5445a7603a43a0676a298063755793c4b2
Orbis2358 <mgkid3310@naver.com>
2019-03-18 15:11:37 +0900

    add TCAS sound file & config

--- addons/orbis_gpws/CfgSounds.hpp
+++ addons/orbis_gpws/CfgSounds.hpp
@@ -4,9 +4,12 @@
 #define F16_VOL_LOW 6
 #define RITA_VOL 15
 #define RITA_VOL_LOW 4
+#define TCAS_VOL 15
+#define TCAS_VOL_LOW 4
 
 class CfgSounds {
 	#include "sound_b747.hpp"
 	#include "sound_f16.hpp"
 	#include "sound_rita.hpp"
+	#include "sound_TCAS.hpp"
 };

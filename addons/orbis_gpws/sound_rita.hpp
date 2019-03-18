195905f0e20c60153aff9539350b86753bfd2e65
Orbis2358 <mgkid3310@naver.com>
2019-03-18 15:11:52 +0900

    update folder names

--- addons/orbis_gpws/sound_rita.hpp
+++ addons/orbis_gpws/sound_rita.hpp
@@ -1,153 +1,153 @@
 	class rita_altitude { // toolow altitude
 		name = "Rita Aaltitude";
 		length = 2.413;
-		sound[] = {"orbis_gpws\sounds\rita\altitude.ogg", RITA_VOL, 1};
+		sound[] = {"orbis_gpws\sounds\Rita\altitude.ogg", RITA_VOL, 1};
 		titles[] = {};
 	};
 	class rita_altitude_low: rita_altitude {
-		sound[] = {"orbis_gpws\sounds\rita\altitude.ogg", RITA_VOL_LOW, 1};
+		sound[] = {"orbis_gpws\sounds\Rita\altitude.ogg", RITA_VOL_LOW, 1};
 	};
 	class rita_angle { // too low pitch angle
 		name = "Rita Angle";
 		length = 5.092;
-		sound[] = {"orbis_gpws\sounds\rita\angle.ogg", RITA_VOL, 1};
+		sound[] = {"orbis_gpws\sounds\Rita\angle.ogg", RITA_VOL, 1};
 		titles[] = {};
 	};
 	class rita_angle_low: rita_angle {
-		sound[] = {"orbis_gpws\sounds\rita\angle.ogg", RITA_VOL_LOW, 1};
+		sound[] = {"orbis_gpws\sounds\Rita\angle.ogg", RITA_VOL_LOW, 1};
 	};
 	class rita_ControlsF { // control surface (aileron) damaged
 		name = "Rita ControlsF";
 		length = 2.141;
-		sound[] = {"orbis_gpws\sounds\rita\ControlsF.ogg", RITA_VOL, 1};
+		sound[] = {"orbis_gpws\sounds\Rita\ControlsF.ogg", RITA_VOL, 1};
 		titles[] = {};
 	};
 	class rita_ControlsF_low: rita_ControlsF {
-		sound[] = {"orbis_gpws\sounds\rita\ControlsF.ogg", RITA_VOL_LOW, 1};
+		sound[] = {"orbis_gpws\sounds\Rita\ControlsF.ogg", RITA_VOL_LOW, 1};
 	};
 	class rita_ejectpilot { // ??? maybe too much damage
 		name = "Rita Ejectpilot";
 		length = 1.784;
-		sound[] = {"orbis_gpws\sounds\rita\ejectpilot.ogg", RITA_VOL, 1};
+		sound[] = {"orbis_gpws\sounds\Rita\ejectpilot.ogg", RITA_VOL, 1};
 		titles[] = {};
 	};
 	class rita_ejectpilot_low: rita_ejectpilot {
-		sound[] = {"orbis_gpws\sounds\rita\ejectpilot.ogg", RITA_VOL_LOW, 1};
+		sound[] = {"orbis_gpws\sounds\Rita\ejectpilot.ogg", RITA_VOL_LOW, 1};
 	};
 	class rita_fuel500 { // fuel 500kg
 		name = "Rita Fuel500";
 		length = 2.899;
-		sound[] = {"orbis_gpws\sounds\rita\fuel500.ogg", RITA_VOL, 1};
+		sound[] = {"orbis_gpws\sounds\Rita\fuel500.ogg", RITA_VOL, 1};
 		titles[] = {};
 	};
 	class rita_fuel500_low: rita_fuel500 {
-		sound[] = {"orbis_gpws\sounds\rita\fuel500.ogg", RITA_VOL_LOW, 1};
+		sound[] = {"orbis_gpws\sounds\Rita\fuel500.ogg", RITA_VOL_LOW, 1};
 	};
 	class rita_fuel800 { // fuel 800kg
 		name = "Rita Fuel800";
 		length = 3.012;
-		sound[] = {"orbis_gpws\sounds\rita\fuel800.ogg", RITA_VOL, 1};
+		sound[] = {"orbis_gpws\sounds\Rita\fuel800.ogg", RITA_VOL, 1};
 		titles[] = {};
 	};
 	class rita_fuel800_low: rita_fuel800 {
-		sound[] = {"orbis_gpws\sounds\rita\fuel800.ogg", RITA_VOL_LOW, 1};
+		sound[] = {"orbis_gpws\sounds\Rita\fuel800.ogg", RITA_VOL_LOW, 1};
 	};
 	class rita_fuel1500 { // fuel 1500kg
 		name = "Rita Fuel1500";
 		length = 3.261;
-		sound[] = {"orbis_gpws\sounds\rita\fuel1500.ogg", RITA_VOL, 1};
+		sound[] = {"orbis_gpws\sounds\Rita\fuel1500.ogg", RITA_VOL, 1};
 		titles[] = {};
 	};
 	class rita_fuel1500_low: rita_fuel1500 {
-		sound[] = {"orbis_gpws\sounds\rita\fuel1500.ogg", RITA_VOL_LOW, 1};
+		sound[] = {"orbis_gpws\sounds\Rita\fuel1500.ogg", RITA_VOL_LOW, 1};
 	};
 	class rita_FuelF { // fuel storage damage
 		name = "Rita FuelF";
 		length = 2.255;
-		sound[] = {"orbis_gpws\sounds\rita\FuelF.ogg", RITA_VOL, 1};
+		sound[] = {"orbis_gpws\sounds\Rita\FuelF.ogg", RITA_VOL, 1};
 		titles[] = {};
 	};
 	class rita_FuelF_low: rita_FuelF {
-		sound[] = {"orbis_gpws\sounds\rita\FuelF.ogg", RITA_VOL_LOW, 1};
+		sound[] = {"orbis_gpws\sounds\Rita\FuelF.ogg", RITA_VOL_LOW, 1};
 	};
 	class rita_fuellow { // low fuel
 		name = "Rita Fuellow";
 		length = 2.123;
-		sound[] = {"orbis_gpws\sounds\rita\fuellow.ogg", RITA_VOL, 1};
+		sound[] = {"orbis_gpws\sounds\Rita\fuellow.ogg", RITA_VOL, 1};
 		titles[] = {};
 	};
 	class rita_fuellow_low: rita_fuellow {
-		sound[] = {"orbis_gpws\sounds\rita\fuellow.ogg", RITA_VOL_LOW, 1};
+		sound[] = {"orbis_gpws\sounds\Rita\fuellow.ogg", RITA_VOL_LOW, 1};
 	};
 	class rita_LEF { // left engine damage
 		name = "Rita LEF";
 		length = 2.569;
-		sound[] = {"orbis_gpws\sounds\rita\LEF.ogg", RITA_VOL, 1};
+		sound[] = {"orbis_gpws\sounds\Rita\LEF.ogg", RITA_VOL, 1};
 		titles[] = {};
 	};
 	class rita_LEF_low: rita_LEF {
-		sound[] = {"orbis_gpws\sounds\rita\LEF.ogg", RITA_VOL_LOW, 1};
+		sound[] = {"orbis_gpws\sounds\Rita\LEF.ogg", RITA_VOL_LOW, 1};
 	};
 	class rita_online { // system online
 		name = "Rita Online";
 		length = 1.069;
-		sound[] = {"orbis_gpws\sounds\rita\online.ogg", RITA_VOL, 1};
+		sound[] = {"orbis_gpws\sounds\Rita\online.ogg", RITA_VOL, 1};
 		titles[] = {};
 	};
 	class rita_online_low: rita_online {
-		sound[] = {"orbis_gpws\sounds\rita\online.ogg", RITA_VOL_LOW, 1};
+		sound[] = {"orbis_gpws\sounds\Rita\online.ogg", RITA_VOL_LOW, 1};
 	};
 	class rita_overload { // high G
 		name = "Rita Overload";
 		length = 2.849;
-		sound[] = {"orbis_gpws\sounds\rita\overload.ogg", RITA_VOL, 1};
+		sound[] = {"orbis_gpws\sounds\Rita\overload.ogg", RITA_VOL, 1};
 		titles[] = {};
 	};
 	class rita_overload_low: rita_overload {
-		sound[] = {"orbis_gpws\sounds\rita\overload.ogg", RITA_VOL_LOW, 1};
+		sound[] = {"orbis_gpws\sounds\Rita\overload.ogg", RITA_VOL_LOW, 1};
 	};
 	class rita_pullUp { // pull up
 		name = "Rita Pullup";
 		length = 1.750;
-		sound[] = {"orbis_gpws\sounds\rita\pullup.ogg", RITA_VOL, 1};
+		sound[] = {"orbis_gpws\sounds\Rita\pullup.ogg", RITA_VOL, 1};
 		titles[] = {};
 	};
 	class rita_pullUp_low: rita_pullUp {
-		sound[] = {"orbis_gpws\sounds\rita\pullup.ogg", RITA_VOL_LOW, 1};
+		sound[] = {"orbis_gpws\sounds\Rita\pullup.ogg", RITA_VOL_LOW, 1};
 	};
 	class rita_REF { // right engine damage
 		name = "Rita REF";
 		length = 2.609;
-		sound[] = {"orbis_gpws\sounds\rita\REF.ogg", RITA_VOL, 1};
+		sound[] = {"orbis_gpws\sounds\Rita\REF.ogg", RITA_VOL, 1};
 		titles[] = {};
 	};
 	class rita_REF_low: rita_REF {
-		sound[] = {"orbis_gpws\sounds\rita\REF.ogg", RITA_VOL_LOW, 1};
+		sound[] = {"orbis_gpws\sounds\Rita\REF.ogg", RITA_VOL_LOW, 1};
 	};
 	class rita_speed { // overspeed
 		name = "Rita Speed";
 		length = 1.901;
-		sound[] = {"orbis_gpws\sounds\rita\speed.ogg", RITA_VOL, 1};
+		sound[] = {"orbis_gpws\sounds\Rita\speed.ogg", RITA_VOL, 1};
 		titles[] = {};
 	};
 	class rita_speed_low: rita_speed {
-		sound[] = {"orbis_gpws\sounds\rita\speed.ogg", RITA_VOL_LOW, 1};
+		sound[] = {"orbis_gpws\sounds\Rita\speed.ogg", RITA_VOL_LOW, 1};
 	};
 	class rita_spodam { // ???
 		name = "Rita Spodam";
 		length = 1.915;
-		sound[] = {"orbis_gpws\sounds\rita\spodam.ogg", RITA_VOL, 1};
+		sound[] = {"orbis_gpws\sounds\Rita\spodam.ogg", RITA_VOL, 1};
 		titles[] = {};
 	};
 	class rita_spodam_low: rita_spodam {
-		sound[] = {"orbis_gpws\sounds\rita\spodam.ogg", RITA_VOL_LOW, 1};
+		sound[] = {"orbis_gpws\sounds\Rita\spodam.ogg", RITA_VOL_LOW, 1};
 	};
 	class rita_SysF { // avionics system damage
 		name = "Rita SysF";
 		length = 1.724;
-		sound[] = {"orbis_gpws\sounds\rita\SysF.ogg", RITA_VOL, 1};
+		sound[] = {"orbis_gpws\sounds\Rita\SysF.ogg", RITA_VOL, 1};
 		titles[] = {};
 	};
 	class rita_SysF_low: rita_SysF {
-		sound[] = {"orbis_gpws\sounds\rita\SysF.ogg", RITA_VOL_LOW, 1};
+		sound[] = {"orbis_gpws\sounds\Rita\SysF.ogg", RITA_VOL_LOW, 1};
 	};

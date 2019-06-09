5d4ae68bec92548dc2db4838c454ca7f2b73a3f7
Mingi Park <mgkid3310@naver.com>
2019-06-09 19:45:46 +0900

    use hard tabs

--- addons/gpws/script_component.hpp
+++ addons/gpws/script_component.hpp
@@ -6,11 +6,11 @@
 // #define DISABLE_COMPILE_CACHE
 
 #ifdef DEBUG_ENABLED_GPWS
-    #define DEBUG_MODE_FULL
+	#define DEBUG_MODE_FULL
 #endif
 
 #ifdef DEBUG_SETTINGS_GPWS
-    #define DEBUG_SETTINGS DEBUG_SETTINGS_GPWS
+	#define DEBUG_SETTINGS DEBUG_SETTINGS_GPWS
 #endif
 
 #include "\z\orbis\addons\main\script_macros.hpp"

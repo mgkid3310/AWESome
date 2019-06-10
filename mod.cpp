86f3043222c3b4c8617ff98574ce851996d78aac
Orbis2358 <mgkid3310@naver.com>
2019-06-11 00:44:42 +0900

    Update mod.cpp

--- mod.cpp
+++ mod.cpp
@@ -1,6 +1,6 @@
-name = "AWESome";
+name = "AWESome : Aerial Warfare Enhanced Somehow";
+actionName = "GitHub";
 action = "https://github.com/mgkid3310/AWESome";
-tooltipOwned = "Aerial Warfare Enhanced Somehow";
-overview = "";
-hideName = 0;
-hidePicture = 0;
+tooltip = "AWESome";
+tooltipOwned = "AWESome";
+overview = "Enhances the ARMA 3's flight environments to be more realistic (Aerodynamics, GPWS, ATC, etc.). Recommended to use with ACE since AWESome will take advantage of ACE when able such as ACE Interactions, ACE Weather and more.";
